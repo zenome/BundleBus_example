a97e770ee5e70f26fbb3c8e99cfe93e901745457
yongho.park@zenome.co.kr <yongho.park@zenome.co.kr>
2016-09-23 11:15:41 +0900

    added bundlebus-client project
    added bundlebus-client project to the iOS example project

--- ios/BundleBusExample/AppDelegate.m
+++ ios/BundleBusExample/AppDelegate.m
@@ -12,25 +12,43 @@
 #import "RCTBundleURLProvider.h"
 #import "RCTRootView.h"
 
+#import "Bundlebus.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  NSString *appKey = @"YOUR_APP_KEY";
+  NSString *appModuleName = @"YOUR_MODULE_NAME";
+  
+  BundleBus *bundlebus = [[BundleBus alloc] init];
+  [bundlebus silentUpdate:appKey];
+  
   NSURL *jsCodeLocation;
-
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"BundleBusExample"
-                                               initialProperties:nil
-                                                   launchOptions:launchOptions];
+  jsCodeLocation = [bundlebus bundleUrl:appKey];
+  RCTRootView *rootView;
+  if(jsCodeLocation != nil) {
+    rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                        moduleName:appModuleName
+                                                 initialProperties:nil
+                                                     launchOptions:launchOptions];
+  }
+  else {
+    jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil]; 
+    rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                        moduleName:appModuleName
+                                                 initialProperties:nil
+                                                     launchOptions:launchOptions];
+  }
+  
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
+  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
   return YES;
 }
 
