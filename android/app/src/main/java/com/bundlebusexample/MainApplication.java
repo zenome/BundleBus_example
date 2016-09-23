be725ce4f02e96af8efdc36ff197b64044e229bb
YongHo Seo <yongho.seo@zenome.co.kr>
2016-09-23 17:28:58 +0900

    Updated Android native part

--- android/app/src/main/java/com/bundlebusexample/MainApplication.java
+++ android/app/src/main/java/com/bundlebusexample/MainApplication.java
@@ -1,6 +1,9 @@
 package com.bundlebusexample;
 
 import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
@@ -8,24 +11,41 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.zenome.bundlebus.BundleBus;
 import com.zenome.bundlebus.BundleBusPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+  private String mAppKey = "1da2b694-e320-4764-95c1-503dbe99da43";
+  private String mServer = "http://192.168.0.15:3000";
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public ReactInstanceManager getReactInstanceManager() {
+      if (!BundleBus.Get().isInitialized()) {
+        BundleBus.Get().init(this.getApplication().getApplicationContext(), "0", mAppKey, mServer);
+      }
+
+      return super.getReactInstanceManager();
+    }
+
     @Override
     protected boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
 
     @Override
+    protected String getJSBundleFile() {
+      return BundleBus.Get().getValidBundlePath(mAppKey);
+    }
+
+    @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new BundleBusPackage()
+          new BundleBusPackage(mAppKey, mServer)
       );
     }
   };
