db6fa0878de14b344225bbaaeb82f7ab72152fcd
YongHo Seo <yongho.seo@zenome.co.kr>
2016-09-23 17:15:29 +0900

    Update Android example

--- index.android.js
+++ index.android.js
@@ -15,8 +15,6 @@ import {
   View
 } from 'react-native';
 
-import BundleBus from 'BundleBus-client';
-
 class BundleBusExample extends Component {
   constructor(props) {
     super(props);
@@ -24,32 +22,8 @@ class BundleBusExample extends Component {
     let packageJson = require('./package.json');
 
     this.state = {
-      updateType : 'None',
-      error : 0,
-      errDesc : '-',
       version : packageJson.version
     }
-
-
-    this.setUpdateType = this.setUpdateType.bind(this);
-    this.setErrorType = this.setErrorType.bind(this);
-  }
-
-  componentWillMount() {
-    BundleBus.init("http://192.168.0.15:3000", this.state.version);
-    BundleBus.checkUpdate("d97f2e3c-8d2e-4bb7-ae16-7e082d726fd3", 
-        this.setUpdateType,
-        this.setErrorType);
-  }
-
-  setUpdateType(aType) {
-    console.log('called setUpdateType');
-    this.setState({updateType: aType});
-  }
-
-  setErrorType(aErrorNo, aReason) {
-    console.log('called setErrorType');
-    this.setState({error : aErrorNo, errDesc: aReason});
   }
 
   render() {
@@ -58,19 +32,8 @@ class BundleBusExample extends Component {
         <Image 
           source={require('./resources/schedulelist_top_icon.png')} />
         <Text style={styles.welcome}>
-          BundleBus Client!
+          BundleBus Client! Version - {this.state.version}
         </Text>
-        <View style={{alignSelf: 'center', margin: 10}}>
-          <Text style={styles.instructions}>App version : {this.state.version}</Text>
-          <Text style={styles.instructions}>UpdateType - {this.state.updateType}</Text>
-          <Text style={styles.instructions}>Error No. - {this.state.errorNo}</Text>
-          <Text style={styles.instructions}>Error reason - {this.state.errDesc}</Text>
-          <Text style={styles.instructions}>
-            If the UpdateType is not 'None', restart current app.{'\n'}
-            Then, you can see the updated app.{'\n'}
-            Be sure that you have to change version field in package.json when you commit.
-          </Text>
-        </View>
       </View>
     );
   }
